62cdaaacb5981d46cc3ebd54b1c014810dd1e378
Chris Peck <chris@minorshift.com>
2019-10-28 12:58:28 +0000

    Added variables.go

---
+++ variables/variables.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s1, s2, s3 string = "String1", "String2", "String4"
+	var i1, i2, i3 int = 1, 2, 3
+	fmt.Println(s1, s2, s3)
+	fmt.Println(i1, i2, i3)
+}
